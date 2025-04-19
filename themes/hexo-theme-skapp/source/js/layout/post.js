8a87ee22cca15bff32bbeb3ff8198640bd67b729
屈定 <niudear@foxmail.com>
2025-04-19 11:04:23 +0800

    1. 简化动画展示

--- themes/hexo-theme-skapp/source/js/layout/post.js
+++ themes/hexo-theme-skapp/source/js/layout/post.js
@@ -1,59 +1,25 @@
 window.addEventListener('load', function() {
-    function addListener(callback) {
-        var 
-            timer = null,
-            requestAnimationFrame = window.requestAnimationFrame 
-                || window.mozRequestAnimationFrame
-                || window.webkitRequestAnimationFrame
-                || window.msRequestAnimationFrame,
-            cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
 
-        function lintener() {
-            cancelAnimationFrame(timer);
+    var posts = document.querySelectorAll('article.page__mini-article');
 
-            timer = requestAnimationFrame(callback.bind(null, function() {
-                document.removeEventListener('scroll', lintener);
-            }));
-        }
+    posts.forEach(function(post) {
+        if (post.parentElement.parentElement.classList.contains('js-hidden')) return;
 
-        document.addEventListener('scroll', lintener);
+        var pack = new Pack(post);
 
-        lintener();
-    }
+        pack
+            .base('js-ease-out-leave-active')
+            .base('js-ease-out-leave')
+            .transfrom('js-ease-out-enter-active')
+            .end(function () {
+                var arr = ['js-ease-out-enter', 'js-ease-out-enter-active', 'js-ease-out-leave', 'js-ease-out-leave-active'];
 
-    window._skappPostAnimation = function() {
-        var posts = document.querySelectorAll('article.page__mini-article');
-        
-        posts.forEach(function(post) {
-            if (post.parentElement.parentElement.classList.contains('js-hidden')) return;
+                arr.forEach(function (item) {
+                    post.classList.remove(item);
+                });
+            })
+            .toggle();
+    });
 
-            var position = getPosition(post);
-
-            var pack = new Pack(post);
-
-            pack
-                .base('js-ease-out-leave-active')
-                .base('js-ease-out-leave')
-                .transfrom('js-ease-out-enter-active')
-                .end(function() {
-                    var arr = ['js-ease-out-enter', 'js-ease-out-enter-active', 'js-ease-out-leave', 'js-ease-out-leave-active'];
-
-                    arr.forEach(function(item) {
-                        post.classList.remove(item);
-                    });
-                })
-
-            addListener(function(remove) {
-                var diff = position.y - window.scrollY - document.documentElement.clientHeight;
-
-                if (diff < 50) {
-                    remove();
-
-                    pack.toggle();
-                }
-            });
-        });
-    }
-
-    window._skappPostAnimation();
 });
+
